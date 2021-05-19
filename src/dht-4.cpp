5d31ef73ef5774bdb5c89c73a469dd8944f2125f
kumar <kumaravel.rajan@tum.de>
2021-05-19 15:32:05 +0200

    Added command line parameters.

--- src/dht-4.cpp
+++ src/dht-4.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 #include "api.h"
 
-int main()
+int ParseConfigFile(char *ConfigFilePath);
+
+int main(int argc, char* argv[])
 {
     std::cout << "Hello, World!" << std::endl;
+
+    if(argc < 2)
+    {
+        std::cout << "No configuration file path given. Program failed.";
+        return -1;
+    }
+    else if(argc == 2)
+    {
+        // configuration file path given.
+        ParseConfigFile(argv[1]);
+    }
+
     return 0;
+}
+
+int ParseConfigFile(char *ConfigFilePath)
+{
+
 }
