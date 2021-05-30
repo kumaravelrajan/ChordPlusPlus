652e68e368fcebe41e0534cc1dfab12c5436a130
Maxi <ge36yog@mytum.de>
2021-05-30 17:45:16 +0200

    [API] add cancellation token, the request handler has to periodically check that

--- src/api/main.cpp
+++ src/api/main.cpp
@@ -5,12 +5,13 @@
 #include <iostream>
 #include <chrono>
 #include <csignal>
+#include <cstdint>
 #include "api.h"
 #include "request.h"
 
 bool sigIntReceived = false;
 
-void sigHandler(sig_t)
+void sigHandler(int)
 {
     sigIntReceived = true;
     std::cout << "Shutting down..." << std::endl;
@@ -19,12 +20,13 @@ void sigHandler(sig_t)
 int main()
 {
     using namespace std::chrono_literals;
+    using namespace util::constants;
     using namespace API;
 
     struct sigaction sigIntHandler
     {
     };
-    sigIntHandler.sa_handler = reinterpret_cast<__sighandler_t>(sigHandler);
+    sigIntHandler.sa_handler = sigHandler;
     sigaction(SIGINT, &sigIntHandler, nullptr);
 
     std::cout << "This is the main method for testing api!" << std::endl;
@@ -32,13 +34,17 @@ int main()
         auto api = std::make_unique<Api>();
 
         // This is a placeholder request handler, it just echoes the request back, as long as it has the correct format.
-        api->on<util::constants::DHT_GET>([](const Message_KEY &message_data) {
+        api->on<DHT_GET>([](const Message_KEY &message_data, auto &cancelled) {
             std::cout << "[apiMain] DHT_GET" << std::endl;
+            for (uint8_t i { 0 }; !cancelled && i < 10; ++i)
+                std::this_thread::sleep_for(1s);
             return message_data.m_bytes;
         });
 
-        api->on<util::constants::DHT_PUT>([](const Message_KEY_VALUE &message_data) {
+        api->on<DHT_PUT>([](const Message_KEY_VALUE &message_data, auto &cancelled) {
             std::cout << "[apiMain] DHT_PUT" << std::endl;
+            for (uint8_t i { 0 }; !cancelled && i < 10; ++i)
+                std::this_thread::sleep_for(1s);
             return message_data.m_bytes;
         });
 
