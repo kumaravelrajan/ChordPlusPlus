da3c1c100eee5d39d7da76ad8a08afcfcbc70974
Kumaravel Rajan <kumaravel.rajan@tum.de>
2021-08-19 09:49:50 +0200

    [Replication] Added range checks for replication vector.

--- src/dht/NodeInformation.cpp
+++ src/dht/NodeInformation.cpp
@@ -179,7 +179,11 @@ void NodeInformation::setReplicationIndex(const uint8_t &replicationIndex){
 }
 std::optional<uint8_t> NodeInformation::getAverageReplicationIndex() const
 {
-    return static_cast<uint8_t>((std::accumulate(NodeInformation::m_allReplicationIndices.begin(), NodeInformation::m_allReplicationIndices.end(), static_cast<uint8_t>(0))) / NodeInformation::m_allReplicationIndices.size());
+    if(m_allReplicationIndices.size() > 0){
+        return static_cast<uint8_t>((std::accumulate(NodeInformation::m_allReplicationIndices.begin(), NodeInformation::m_allReplicationIndices.end(), static_cast<uint8_t>(0))) / NodeInformation::m_allReplicationIndices.size());
+    } else {
+        return static_cast<uint8_t>(0);
+    }
 }
 
 // Node Methods:
